649502d2497d07f66e881ab65b135949e1ad9193
TarunVanshkar <tarunben33@gmail.com>
2023-07-01 21:06:59 +0530

    Contest

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "../Components/Products";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h2 className="heading">All Items</h2>
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
